e1fd03b1bd3e1f8b8f8f8706ece901cfbbdab464
elir-mahad <mmahad280@gmail.com>
2022-02-22 09:11:13 -0500

    enhance(weather): add useEffect hook to fetch api data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //
 
 function Weather() {
@@ -8,6 +8,15 @@ function Weather() {
     //
     const API_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=current,minutely,hourly,alerts&appid=37f731d78f30eee5072bbb693ddacb40";
     // 
+    useEffect(() => {
+        fetch(API_URL)
+          .then((response) => response.json())
+          .then((data) => {
+            setWeath(data);
+          })
+          .catch((error) => setError(error));
+      }, []);
+      //
     return (
         <div>
             <h1>Weather</h1>
