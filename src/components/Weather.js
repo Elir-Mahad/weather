77e259b27cb23328abfcc7ba886a40bbae050a23
elir-mahad <mmahad280@gmail.com>
2022-02-22 09:18:00 -0500

    style(weather): turn prettier on, format code

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,29 +2,30 @@ import React, { useState, useEffect } from "react";
 //
 
 function Weather() {
-    //
-    const [weath, setWeath] = useState();
-    const [error, setError] = useState();
-    //
-    const API_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=current,minutely,hourly,alerts&appid=37f731d78f30eee5072bbb693ddacb40";
-    // 
-    useEffect(() => {
-        fetch(API_URL)
-          .then((response) => response.json())
-          .then((data) => {
-            setWeath(data);
-          })
-          .catch((error) => setError(error));
-      }, []);
-    //
-      if (weath === undefined) return <h1>Loading</h1>;
-      if (error) console.log(error);
-    //   
-    return (
-        <div>
-            <h1>Weather</h1>
-        </div>
-    )
+  //
+  const [weath, setWeath] = useState();
+  const [error, setError] = useState();
+  //
+  const API_URL =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=current,minutely,hourly,alerts&appid=37f731d78f30eee5072bbb693ddacb40";
+  //
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeath(data);
+      })
+      .catch((error) => setError(error));
+  }, []);
+  //
+  if (weath === undefined) return <h1>Loading</h1>;
+  if (error) console.log(error);
+  //
+  return (
+    <div>
+      <h1>Weather</h1>
+    </div>
+  );
 }
 
-export default Weather
+export default Weather;
