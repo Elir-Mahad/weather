a2395a0ff303e88c1d72f527120e023e548a508e
elir-mahad <mmahad280@gmail.com>
2022-02-23 11:19:32 -0500

    enhance(errordisplay): pass in error prop

--- src/components/ErrorDisplay/ErrorDisplay.js
+++ src/components/ErrorDisplay/ErrorDisplay.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function ErrorDisplay() {
+function ErrorDisplay({ error }) {
   return (
     <div>
       <h2>Something went wrong</h2>
+      <p>{error}</p>
     </div>
   );
 }
