242d879a4573eb0e3f06ce20da63c11b5efe51ce
elir-mahad <mmahad280@gmail.com>
2022-02-22 20:55:51 -0500

    enhance(useweatherdata): add isdesiredindex funct to output 1 temp per day

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -1,3 +1,11 @@
 import { useState, useEffect } from "react";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+
+/**
+ * The weather data comes with 8 3h forcast per day.
+ * We only care about one per day.
+ * so we can use this function to filter only what we want.
+ */
+
+const isDesiredIndex = (_, i) => i % 8 === 0;
