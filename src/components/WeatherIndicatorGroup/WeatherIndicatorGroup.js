da9ec0aff6c9890dca24b97ad5106e31c5eabbd2
elir-mahad <mmahad280@gmail.com>
2022-02-23 10:48:53 -0500

    feat(components): create weatherindicatorgroup folder and file

---
+++ src/components/WeatherIndicatorGroup/WeatherIndicatorGroup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WeatherIndicatorGroup() {
+  return (
+    <div>
+      <h1>Weather indicator group</h1>
+    </div>
+  );
+}
+
+export default WeatherIndicatorGroup;
